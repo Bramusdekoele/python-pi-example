5edf17be131490249930870f8afbac39dc1df144
Bram <bram@pointybracket.net>
2020-04-23 16:42:24 +0200

    Newest commit

--- pi/main.py
+++ pi/main.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
 def main(msg):
-
+    // Another comment
     // Adding a second comment to this file.
-// Print a message
+    // Print a message
 print(msg)
 
 main("hello people")
